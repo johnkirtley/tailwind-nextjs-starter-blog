5c7874601f1e621d8faa6b7920189be215ba55af
johnkirtley <kirtleyj16@gmail.com>
2024-09-02 10:29:02 -0700

    updates

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Random Exercises To Improve Posture',
+  author: 'John',
+  headerTitle: 'Random Exercises To Improve Posture',
+  description: 'A tool that generates exercises and stretches to help improve posture',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
