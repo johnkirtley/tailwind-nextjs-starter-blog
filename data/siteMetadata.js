2a200b5d6be1e43c31b6d3cbc66c252854dc14d2
johnkirtley <kirtleyj16@gmail.com>
2024-09-02 18:14:21 -0700

    updates

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'A tool that generates exercises and stretches to help improve posture',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://exercisestoimproveposture.com',
+  siteUrl: 'https://www.exercisestoimproveposture.com',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
